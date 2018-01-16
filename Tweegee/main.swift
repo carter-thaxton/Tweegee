8b45226c00d969a17871508e3460af84f5aa1d8b
Carter Thaxton <carter.thaxton@gmail.com>
2018-01-16 11:19:53 -0600

    Initial Commit

---
+++ Tweegee/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tweegee
+//
+//  Created by Carter Thaxton on 1/16/18.
+//  Copyright © 2018 Carter Thaxton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
