ec6fcd8d6f42efcd3ba23d43ff68b7b091abe5ff
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-05 23:33:49 -0800

    Implement lexer stage of parser

--- Tweegee/TweeParser.swift
+++ Tweegee/TweeParser.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 
+enum TweeError : Error {
+    case SyntaxError
+}
+
 enum TweeToken {
     case Newline
     case Passage(name: String, tags: [String], position: CGPoint?)
     case Text(String)
     case Link(name: String, title: String?)
-    case Macro(name: String)  // TODO: add other aspects of macro
+    case Macro(name: String?, expr: String?)
+    case Comment(String)
 }
 
 extension TweeToken : Equatable {
@@ -28,8 +33,10 @@ extension TweeToken : Equatable {
             return text == text2
         case (.Link(let name, let title), .Link(let name2, let title2)):
             return name == name2 && title == title2
-        case (.Macro(let name), .Macro(let name2)):
-            return name == name2
+        case (.Macro(let name, let expr), .Macro(let name2, let expr2)):
+            return name == name2 && expr == expr2
+        case (.Comment(let comment), .Comment(let comment2)):
+            return comment == comment2
         default:
             return false
         }
@@ -57,22 +64,29 @@ private let passageHeaderRegex = try! NSRegularExpression(pattern:
         )
     """, options: .allowCommentsAndWhitespace)
 
-private let bracketsCharacterSet = CharacterSet(charactersIn: "[<")
+private let bracketsCharacterSet = CharacterSet(charactersIn: "[</")
 
 
 class TweeParser {
     func parse(filename: String, block: @escaping (TweeToken) -> Void) throws {
         let str = try String(contentsOfFile: filename, encoding: .utf8)
-        parse(string: str, block: block)
+        try parse(string: str, block: block)
     }
     
-    func parse(string: String, block: @escaping (TweeToken) -> Void) {
-        string.enumerateLines { line, _ in
-            self.parse(line: line, block: block)
+    func parse(string: String, block: @escaping (TweeToken) -> Void) throws {
+        var err : Error? = nil
+        string.enumerateLines { line, stop in
+            do {
+                try self.parse(line: line, block: block)
+            } catch {
+                stop = true
+                err = error
+            }
         }
+        if err != nil { throw err! }
     }
 
-    func parse(line: String, block handleToken: (TweeToken) -> Void) {
+    func parse(line: String, block handleToken: (TweeToken) -> Void) throws {
         if let matches = line.match(regex: passageHeaderRegex) {
             let name = matches[1]!.trimmingCharacters(in: .whitespaces)
             let tags = matches[2]
@@ -99,14 +113,15 @@ class TweeParser {
                         accText += str!!
                     }
                     if !s.isAtEnd {
-                        if s.match("[[") {
+                        if s.match("[[") {  // link, e.g [[Choice|choice_1]]
                             if !accText.isEmpty {
                                 handleToken(.Text(accText))
                                 accText = ""
                             }
                             let link = try? s.scan(upTo: "]]")
                             if link != nil && link! != nil {
-                                let nameAndTitle = link!!.components(separatedBy: "|")
+                                // split on pipe, and trim each component
+                                let nameAndTitle = link!!.split(separator: "|", maxSplits: 1).map { $0.trimmingCharacters(in: .whitespaces) }
                                 if nameAndTitle.count >= 2 {
                                     handleToken(.Link(name: nameAndTitle[1], title: nameAndTitle[0]))
                                 } else {
@@ -115,13 +130,40 @@ class TweeParser {
                                 s.match("]]")
                             } else {
                                 // Invalid link syntax
+                                throw TweeError.SyntaxError
                             }
-                        } else if s.match("<<") {
+                        } else if s.match("<<") {  // macro, e.g. <<set $i = 5>>
                             if !accText.isEmpty {
                                 handleToken(.Text(accText))
                                 accText = ""
                             }
+                            let macro = try? s.scan(upTo: ">>")
+                            if macro != nil && macro! != nil {
+                                let text = macro!!.trimmingCharacters(in: .whitespaces)
+                                let nameAndExpr = text.split(separator: " ", maxSplits: 1)
+                                if nameAndExpr.count >= 2 {
+                                    handleToken(.Macro(name: String(nameAndExpr[0]), expr: String(nameAndExpr[1])))
+                                } else if nameAndExpr[0].starts(with: "$") {
+                                    handleToken(.Macro(name: nil, expr: String(nameAndExpr[0])))
+                                } else {
+                                    handleToken(.Macro(name: String(nameAndExpr[0]), expr: nil))
+                                }
+                                s.match(">>")
+                            } else {
+                                // Invalid macro syntax
+                                throw TweeError.SyntaxError
+                            }
+                        } else if s.match("//") {  // comment, e.g. // here's a comment
+                            if !accText.isEmpty {
+                                handleToken(.Text(accText))
+                                accText = ""
+                            }
+                            s.match("//")
+                            let comment = s.remainder.trimmingCharacters(in: .whitespaces)
+                            handleToken(.Comment(comment))
+                            s.peekAtEnd()
                         } else {
+                            try! s.back()
                             accText += String(try! s.scanChar())
                         }
                     }
--- TweegeeTests/TweegeeParserTests.swift
+++ TweegeeTests/TweegeeParserTests.swift
@@ -10,28 +10,62 @@ import XCTest
 
 class TweegeeParserTests: XCTestCase {
 
-    func testParse() {
+    func testBasicParse() {
         checkParse(string: """
+            Some normal text
+            Brackets, like I <3 U, and/or [this] thing
             Text [[link]] text
-            [[Choice 1|choice_1]] | [[Choice 2|choice_2]]
+            <<set $i = 5>>
+            <<if $i > 0 >>I have <<$i>> items<<else>>No items<<endif>>
+            [[Choice 1|choice_1]] | [[ Choice 2 | choice_2 ]]
+            // Comment is ignored
         """,
         tokens: [
+            .Text("Some normal text"),
+            .Newline,
+            .Text("Brackets, like I <3 U, and/or [this] thing"),
+            .Newline,
             .Text("Text "),
             .Link(name: "link", title: nil),
             .Text(" text"),
             .Newline,
+            .Macro(name: "set", expr: "$i = 5"),
+            .Newline,
+            .Macro(name: "if", expr: "$i > 0"),
+            .Text("I have "),
+            .Macro(name: nil, expr: "$i"),
+            .Text(" items"),
+            .Macro(name: "else", expr: nil),
+            .Text("No items"),
+            .Macro(name: "endif", expr: nil),
+            .Newline,
             .Link(name: "choice_1", title: "Choice 1"),
             .Text(" | "),
             .Link(name: "choice_2", title: "Choice 2"),
-            .Newline
+            .Newline,
+            .Comment("Comment is ignored"),
+            .Newline,
         ])
     }
     
+    func testMissingEnd() {
+        checkParseFails(string: """
+            Missing [[Choice
+        """)
+    }
+
+    
+    // MARK: Helper methods
+
     func checkParse(string: String, tokens: [TweeToken]) {
         var result = [TweeToken]()
         let p = TweeParser()
-        p.parse(string: string) { result.append($0) }
+        try! p.parse(string: string) { result.append($0) }
         XCTAssertEqual(result, tokens)
     }
 
+    func checkParseFails(string: String) {
+        let p = TweeParser()
+        XCTAssertThrowsError(try p.parse(string: string) { _ in } )
+    }
 }
